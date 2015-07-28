9c52def9f11fab0fb1568ecf57a3e9b343e9cdc4
sweemeng <sweester@gmail.com>
2015-07-28 10:58:21 +0800

    Commit template, make queue name for specific to location + isp

--- blockornot/app.py
+++ blockornot/app.py
@@ -9,4 +9,6 @@ def create_app():
     app.config["CELERY_BROKER_URL"] = config["CELERY_BROKER_URL"]
     app.config["CELERY_RESULT_BACKEND"] = config["CELERY_RESULT_BACKEND"]
     app.config["CELERY_IMPORTS"] = ("worker")
+    app.config["LOCATIONS"] = config["LOCATIONS"]
+    app.config["DNS_TARGETS"] = config["DNS_TARGETS"]
     return app
--- blockornot/server.py
+++ blockornot/server.py
@@ -19,40 +19,39 @@ def index():
     return render_template("index.html")
 
 # Now how do we tidy up this code
-# TODO: Iterate through server list, call worker from different location
 @socketio.on("check http", namespace="/checkhttp")
 def check_http(json):
     data = json
     url = data["url"]
     if not re.match(r"^http\://", url):
         url = "http://%s" % url
-    result = HTTPResult("TM", "Subang Jaya", param=url)
-    result.run()
+    for entry in app.config["LOCATIONS"]:
+        result = HTTPResult(entry["ISP"], entry["location"], param=url)
+        result.run()
 
-    emit("http received", result.to_json())
+        emit("http received", result.to_json())
 
 # Really you should just put it to a task somewhere to push it to socket io
 @socketio.on("http result", namespace="/checkhttp")
 def http_result(json):
     task_id = json["task_id"]
-    result = HTTPResult("TM", "Subang Jaya", task_id=task_id)
+    result = HTTPResult(json["ISP"], json["location"], task_id=task_id)
     result.run()
     emit("http received", result.to_json())
 
-# TODO: Test with different DNS server
-# TODO: Have a list of dns server to test
 @socketio.on("check dns", namespace="/checkdns")
 def check_dns(data):
     url = data["url"]
-    result = DNSResult("8.8.8.8", "google", param=url)
-    result.run()
+    for targets in app.config["DNS_TARGETS"]:
+        result = DNSResult(targets["server"], targets["provider"], param=url)
+        result.run()
 
-    emit("dns received", result.to_json())
+        emit("dns received", result.to_json())
 
 @socketio.on("dns result", namespace="/checkdns")
 def dns_result(data):
     task_id = data["task_id"]
-    result = DNSResult("8.8.8.8", "google", task_id=task_id)
+    result = DNSResult(data["server"], data["provider"], task_id=task_id)
     result.run()
 
     emit("dns received", result.to_json())
