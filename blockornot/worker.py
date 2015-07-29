71db9136f004d15b3259803cbd3a562a22a08848
sweemeng <sweester@gmail.com>
2015-07-29 12:18:03 +0800

    Update worker with more indept test. change utils to result. Update code to reflect output

--- blockornot/worker.py
+++ blockornot/worker.py
@@ -5,33 +5,73 @@ import requests
 import logging
 import subprocess
 import re
+import dns
+import dns.resolver
+import dns.exception
 
 app = create_app()
 
 backend = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 backend.conf.update(app.config)
 
+MCMC_BLOCK_PAGE_PATTERN = re.compile(r"This website is not available in Malaysia as it violates")
+MCMC_BLOCK_PAGE_HEADER = re.compile(r"Makluman/Notification")
+
 # TODO: make it easy for people to expand test. We probably going to use zope.interface on this.
-# TODO: Store result into mongo. Since this runs on client.
-# TODO: On the other hand putting mongodb dependency is a bad idea
+# TODO: How do we create a runnable object anyway?
 
 # Redis support routing. we just assign each instance with it's own queue celery -A worker.backend worker -Q japan
 # The hard part is assigning metadata based on queue name, we probably use a trust system and put it in a config file
+# This should do ore than just running it slowly in the background. it is humiliating
 @backend.task
 def http_task(url):
     r = requests.get(url)
-    return r
+    status_code = r.status_code
+    reason = r.reason
+    if r.status_code == 200:
 
-# TODO: What other error in DNS again?/
+        if MCMC_BLOCK_PAGE_HEADER.search(r.content):
+            if MCMC_BLOCK_PAGE_PATTERN.search(r.content):
+                status_code = 451
+                reason = "Unavailable For Legal Reasons"
+
+    result = (status_code, r.content, reason)
+    return result
+
+# TODO: Local ISP DNS will resolve to wrong IP.
 @backend.task
 def dns_task(url, dns_server=None):
-    command = ["dig", url]
+    resolver = dns.resolver.Resolver()
+    reason = ""
+    status = ""
     if dns_server:
-        server = "@%s" % dns_server
-        command.append(server)
-    p = subprocess.check_output(command)
-    if re.search("NXDOMAIN", p):
-        return "Not Found"
-    if re.search("NOERROR", p):
-        return "Resolved properly"
+        resolver.nameservers = [dns_server]
+    try:
+        answer = resolver.query(url)
+        for entry in answer:
+            ip = entry.to_text()
+            r = requests.get("http://%s" % ip, headers={"Host":url})
 
+            if r.status_code == 200:
+
+                if MCMC_BLOCK_PAGE_HEADER.search(r.content):
+                    if MCMC_BLOCK_PAGE_PATTERN.search(r.content):
+                        reason = "Unavailable For Legal Reasons"
+                        status = "Error"
+                else:
+                    reason = "Content resolved properly"
+                    status = "OK"
+
+    except dns.resolver.NXDOMAIN as e:
+        status = "NXDOMAIN"
+        reason = "Domain cannot be resolved"
+
+    except dns.resolver.Timeout:
+        status = "Timeout"
+        reason = "Timed out trying to resolve %s" % url
+
+    except dns.exception.DNSException:
+        status = "Error"
+        reason = "Unhandled reception, bug the guys to find out"
+
+    return (status, reason)
